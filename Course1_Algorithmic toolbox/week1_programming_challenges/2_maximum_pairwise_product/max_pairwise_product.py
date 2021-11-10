4eb6239682212fced2f95eb24a97b8eabfa4a72f
Vamsi <60150788+vamsi-peddinti@users.noreply.github.com>
2021-11-10 18:09:19 -0500

    Update max_pairwise_product.py

--- Course1_Algorithmic toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Course1_Algorithmic toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,12 +1,16 @@
 def max_pairwise_product(numbers):
     n = len(numbers)
     max_product = 0
+    max_index1=0
     for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+        if max_index1==0 or numbers[first]>numbers[max_index1]:
+            max_index1=first
+    max_index2=0
+    for second in range(n):
+        if second!=max_index1 and (max_index2==0 or numbers[second]>numbers[max_index2]):
+            max_index2=second
 
-    return max_product
+    return  (numbers[max_index1] * numbers[max_index2])
 
 
 if __name__ == '__main__':
