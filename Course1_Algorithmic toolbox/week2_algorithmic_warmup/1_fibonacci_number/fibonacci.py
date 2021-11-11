f5982499719df925bcccd91b589273eddc15715b
Vamsi <60150788+vamsi-peddinti@users.noreply.github.com>
2021-11-10 21:18:57 -0500

    Update fibonacci.py

--- Course1_Algorithmic toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Course1_Algorithmic toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,10 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
-n = int(input())
-print(calc_fib(n))
+n=[]
+length=int(input())
+n.append(0)
+n.append(1)
+sum=0
+for i in range(2,length):
+    n[i]=n[i-1]+n[i-2]
+    sum=n[i]
+print(sum)
