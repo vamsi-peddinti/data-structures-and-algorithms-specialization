5ecb057876a4858bbf6bf428e7c4527161ee574b
Vamsi <60150788+vamsi-peddinti@users.noreply.github.com>
2021-11-13 12:39:28 -0500

    4 done

--- Course1_Algorithmic toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Course1_Algorithmic toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,15 @@
 # Uses python3
 import sys
+def gcd_naive(a, b):
+    if b==0:
+        return a
+    else:
+        remainder=a%b
+        return gcd_naive(b,remainder)
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-
-    return a*b
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    a, b = map(int, input() .split())
+    op=gcd_naive(a,b)
+    print(int((a*b)/gcd_naive(a,b)))
 
--- Course1_Algorithmic toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ Course1_Algorithmic toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,18 @@
 # Uses python3
-import sys
+import numpy as np
+n=[]
+length, mod=map(int,input().split())
+if length==0:
+    n.append(0)
+    ans=0
+else:
+    n.append(0)    
+    n.append(1)
+    if length==1:
+        ans=1
+    for i in range(2,length+1):
+        newelement=(n[i-1]+n[i-2])%mod
+        n.append(newelement)
+    ans=newelement
+print(ans)
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
-
-if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
