d9f58c03ef20b5b2b97a20659cbd6f2858a8142a
Vamsi <60150788+vamsi-peddinti@users.noreply.github.com>
2021-11-14 19:18:30 -0500

    Update fractional_knapsack.py

--- Course1_Algorithmic toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ Course1_Algorithmic toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -3,15 +3,37 @@ import sys
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
+    maxratio=[]
+    length=len(weights)
+    for i in range(length):
+        maxratio.append((values[i]/weights[i]))
 
+    for j in range(len(maxratio)):
+        max_value=max(maxratio)
+        maxind=maxratio.index(max_value)
+        if capacity%weights[maxind]>=0:
+            capop=capacity//weights[maxind]
+            value+=capop*values[maxind]
+        capacity-=capop*weights[maxind]
+        maxratio.remove(max_value)
+        j+=1
+
+
+
+
+
+
+    #print(maxratio, maxind)
+    # write your code here
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, input().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    n, capacity = map(int, input().split())
+    values, weights = [], []
+    for i in range(n):
+        a, b = map(int, input().split())
+        values.append(a)
+        weights.append(b)
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
